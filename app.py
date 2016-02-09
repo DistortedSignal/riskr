81bafbe2108ecc720d6fedb1d134e81e19dae36e
Tom Kelley <tkelley@axolotl.com>
2016-02-09 11:22:57 -0800

    Adding functions to add templates to dict, adding js files to pre-loaded set of files.

--- app.py
+++ app.py
@@ -3,35 +3,54 @@ from flask import Flask, Response
 
 import os
 
+def add_template_to_dict(template_dir, template_name, template_dict):
+    template_dict[template_name] = Template(open(template_dir + os.sep +
+        template_name + '.jinja').read())
+
 # Engineering tradeoff: I REALLY don't want to read these files from disk every
 # time that I need to serve a page, so I'm going to load the files at app
 # initialization.
 def load_templates(template_directory):
     template_dictionary = {}
-    template_dictionary['user'] = Template(open(template_directory + os.sep +
-        'user.jinja').read())
-    template_dictionary['post'] = Template(open(template_directory + os.sep +
-        'post.jinja').read())
-    template_dictionary['container'] = Template(open(template_directory +
-        os.sep + 'container.jinja').read())
-    template_dictionary['comment'] = Template(open(template_directory + os.sep +
-        'comment.jinja').read())
-    template_dictionary['comment_box'] = Template(open(template_directory + 
-        os.sep + 'comment_box.jinja').read())
+    add_template_to_dict(template_directory, 'user', template_dictionary)
+    add_template_to_dict(template_directory, 'post', template_dictionary)
+    add_template_to_dict(template_directory, 'container', template_dictionary)
+    add_template_to_dict(template_directory, 'comment', template_dictionary)
+    add_template_to_dict(template_directory, 'comment_box', template_dictionary)
     return template_dictionary
+
+# Yes, this function uses side effects of adding items to a dict to influence
+# global state. Sue me
+def load_raw_file(file_dir, file_name, file_dict):
+    file_dict[file_name] = unicode(open(file_dir + os.sep + file_name).read())
 
 def load_css(css_directory):
     css_dictionary = {}
     # TODO Switch this based on cmd line flag
-    css_dictionary['bootstrap.css'] = unicode(open(css_directory + os.sep +
-        'bootstrap.css').read())
-    css_dictionary['bootstrap-responsive.css'] = unicode(open(css_directory +
-        os.sep + 'bootstrap-responsive.css').read())
+    load_raw_file(css_directory, 'bootstrap.css', css_dictionary)
+    load_raw_file(css_directory, 'bootstrap-responsive.css', css_dictionary)
     return css_dictionary
 
 def load_js(js_directory):
     js_dictionary = {}
     # TODO Switch this based on cmd line flag
+    load_raw_file(js_directory, 'bootstrap.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-alert.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-button.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-carousel.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-collapse.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-dropdown.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-modal.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-popover.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-scrollspy.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-tab.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-tooltip.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-transition.js', js_dictionary)
+    load_raw_file(js_directory, 'bootstrap-typeahead.js', js_dictionary)
+    load_raw_file(js_directory, 'custom.js', js_dictionary)
+    load_raw_file(js_directory, 'ie10-viewport-bug-workaround.js',
+        js_dictionary)
+    load_raw_file(js_directory, 'jquery.js', js_dictionary)
     return js_dictionary
 
 def get_static_file(content, mime_type):
