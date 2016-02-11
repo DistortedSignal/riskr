98d3ee553a4451aee330e7937f4db4d8d8618e4c
DistortedSignal <distortedsignal@gmail.com>
2016-02-11 08:16:18 -0800

    Adding simple framework for rendering templates for user/comment/post/main page.

---
+++ lib/template_lib.py
@@ -0,0 +1,13 @@
+from jinja2 import Template
+
+def render_user(user_id, template_dict):
+    pass
+
+def render_comment(comment_id, template_dict):
+    pass
+
+def render_post(post_id, template_dict):
+    pass
+
+def render_front_page(user_id, template_dict):
+    pass
