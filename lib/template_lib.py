39425d73f228a904bdd22f68335e38cf027109dd
Tom Kelley <tkelley@axolotl.com>
2016-02-12 12:39:07 -0800

    Sketching out the basic database logic that needs to happen in order to get posts from the db. Attempting to maintain the 'mockout' of the db with basic data structures.

--- lib/template_lib.py
+++ lib/template_lib.py
@@ -1,43 +1,95 @@
 from __future__ import print_function
 
-def render_user(user_id, template_dict):
+def render_user(user_id, template_dict, conn_db):
     # Get User from database based on user id
-    # TODO Like, this.
-    user = {'name': 'Tom'}
+    try:
+        cursor = conn_db.cursor()
+        cursor.execute("SELECT display_name FROM user WHERE id=%(user_id)s",
+            {'user_id': user_id})
+        results = conn_db.fetchall()
+        # TODO Replace this logic once the database is integrated
+        user = {'name': 'display_name'}
+    except TypeError:
+        # To make it so the db being mocked out still works.
+        user = {'name': 'Tom'}
     # Render user
     return template_dict['user'].render(user=user)
 
-def render_comment(comment_id, template_dict):
+def render_comment(comment_id, template_dict, conn_db):
     # Get Comment from database based on comment id
-    # TODO Also, this.
-    comment = {'user_id': 1, 'comment_body': 'This is comment', 
-    'posted_date': '$date_here'}
-    comment['compiled_user'] = render_user(comment['user_id'], template_dict)
+    try:
+        cursor = conn_db.cursor()
+        cursor.execute("SELECT user_id, comment_body, posted_date FROM " +
+            "comment WHERE comment_id=%(comment_id)s", 
+            {'comment_id': comment_id})
+        results = conn_db.fetchall()
+        # TODO Construct comment from results
+        cursor.close()
+    except TypeError:
+        comment = {'user_id': 1, 'comment_body': 'This is comment', 
+        'posted_date': '$date_here'}
+    comment['compiled_user'] = render_user(comment['user_id'], template_dict, 
+        db_conn)
     return template_dict['comment'].render(comment=comment)
 
-def render_post(post_id, template_dict):
+def render_post(post_id, template_dict, conn_db):
     # Get post from database based on post id
-    # TODO
-    post = {'user_id': 1, 'title': 'This is post title',
-    'body': 'This is a super long post body, and it can go on for a long way.',
-    'posted_date': '$date_here'}
+    # TODO Remove try blocks so I can use the same cursor twice.
+    try:
+        cursor = conn_db.cursor()
+        cursor.execute("SELECT user_id, title, body, posted_date FROM post" +
+            "WHERE id=%(post_id)s",
+            {'post_id': post_id})
+        results = conn_db.fetchall()
+        # TODO Construct post from results
+        cursor.close()
+    except TypeError:
+        post = {'user_id': 1, 'title': 'This is post title', 'body': 
+        'This is a super long post body, and it can go on for a long way.',
+        'posted_date': '$date_here'}
 
     # Get comment list from database based on post id
-    # TODO
-    # TODO Change this to a mapping function
-    post['compiled_comments'] = [render_comment(1, template_dict),
-    render_comment(2, template_dict), render_comment(3, template_dict)]
+    try:
+        cursor = conn_db.cursor()
+        cursor.execute("SELECT id FROM comment WHERE post_id=%(post_id)s",
+            {'post_id': post_id})
+        results = conn_db.fetchall()
+        cursor.close()
+    except TypeError:
+        results_transformed = [1,2,3]
+
+    post['compiled_comments'] = map(
+        lambda x: render_comment(x, template_dict, db_conn),
+        results_transformed)
 
     # Get user based on user_id
-    post['compiled_user'] = render_user(post['user_id'], template_dict)
+    post['compiled_user'] = render_user(post['user_id'], template_dict, db_conn)
     return template_dict['post'].render(post=post)
 
-def render_front_page(user_id, template_dict):
+def render_front_page(user_id, template_dict, conn_db):
     # Get User from database based on id
-    user = {'name': 'Tom'}
+    try:
+        cursor = conn_db.cursor()
+        cursor.execute("SELECT display_name FROM user WHERE id=%(user_id)s",
+            {'user_id': user_id})
+        results = conn_db.fetchall()
+        cursor.close()
+    except TypeError:
+        user = {'name': 'Tom'}
 
     # Get a list of posts from the database
-    posts = [render_post(1, template_dict), render_post(2, template_dict),
-    render_post(3, template_dict), render_post(4, template_dict),
-    render_post(5, template_dict)]
+    try:
+        cursor = conn_db.cursor()
+        cursor.execute("SELECT id FROM post") # Limit this in some way, shape,
+        #                                       or form
+        results = db_conn.fetchall()
+        # TODO
+        cursor.close()
+    except TypeError:
+        results_transformed = [1,2,3,4,5]
+
+    posts = map(
+        lambda x: render_post(x, template_dict, db_conn),
+        results_transformed)
+    
     return template_dict['main'].render(compiled_posts=posts, user=user)
