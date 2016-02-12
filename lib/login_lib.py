965ca98ffe5a50c6d5d54788185ddb378dba5b5d
DistortedSignal <distortedsignal@gmail.com>
2016-02-11 23:18:42 -0800

    Sketching out some simple DB functions that will be useful when a database is stood up.

--- lib/login_lib.py
+++ lib/login_lib.py
@@ -1,15 +1,30 @@
 import bcrypt
 
-def create_new_user(user_name, password, email_address):
+def create_new_user(user_name, email_address, password, db_conn):
     salt = bcrypt.gensalt()
     pw_hash = bcrypt.hashpw(password, salt)
-    # TODO Make actual database call that stores user name, salt, pw hash,
-    # and email address
+    # TODO Figure out how to id (either auto-generate or other)
+    cursor = db_conn.cursor()
+    cursor.execute("INSERT INTO user " +
+        "(display_name, email_address, password_hash, salt) VALUES" +
+        "(%s,           %s,            %s,            %s)",
+        ( user_name,    email_address, pw_hash,       salt))
+    db_conn.commit()
+    cursor.close()
 
-    return attempt_login(email_address, password)
+    return attempt_login(email_address, password, db_conn)
 
-def attempt_login(email_address, password):
+def attempt_login(email_address, password, db_conn):
     # TODO Database code to lookup user by email address, get id, salt, pw hash
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT id, password_hash, salt FROM user WHERE " +
+        "email_address = %s", (email_address))
+    row_tuples = cursor.fetchall()
+
+    if row_tuples == []:
+        # There were no users in the db with that email address
+        return False
+
     if bcrypt.hashpw(password, salt) == pw_hash:
         # TODO Generate a temporary token and embed it in the return value
         return user_id
